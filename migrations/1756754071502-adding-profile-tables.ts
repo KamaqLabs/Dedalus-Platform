2a18a9522abf8618f11c4a7589141bdd0328543d
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-09-01 14:19:48 -0500

    chore: add migration for creating administrator and guest profile tables

---
+++ migrations/1756754071502-adding-profile-tables.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TestColumnsUpdated1756754071502 implements MigrationInterface {
+    name = 'TestColumnsUpdated1756754071502'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`administrator_profile\` (\`id\` int NOT NULL AUTO_INCREMENT, \`account_id\` int NOT NULL, \`name\` varchar(255) NOT NULL, \`last_name\` varchar(255) NOT NULL, \`dni\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`phone_number\` varchar(255) NOT NULL, \`hotelId\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`guest_profile\` (\`id\` int NOT NULL AUTO_INCREMENT, \`account_id\` int NOT NULL, \`name\` varchar(255) NOT NULL, \`last_name\` varchar(255) NOT NULL, \`dni\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`phone_number\` varchar(255) NOT NULL, \`status\` enum ('INACTIVE', 'ACTIVE') NOT NULL DEFAULT 'INACTIVE', \`nfc_key\` varchar(255) NOT NULL, \`guestCode\` varchar(255) NOT NULL, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), UNIQUE INDEX \`IDX_44987cf53f2c3935784315315d\` (\`guestCode\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX \`IDX_44987cf53f2c3935784315315d\` ON \`guest_profile\``);
+        await queryRunner.query(`DROP TABLE \`guest_profile\``);
+        await queryRunner.query(`DROP TABLE \`administrator_profile\``);
+    }
+
+}
