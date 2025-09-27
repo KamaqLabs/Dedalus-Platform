dd95719bd6a133682dc0eb4f2e6013ddb98606ac
Valentino Cervantes Erequita <104030868+KiwiAmenazante@users.noreply.github.com>
2025-09-26 22:40:25 -0500

    feat: enhance AdministratorProfile with hotelId and timestamp fields, and update personal information method

--- src/profiles/domain/model/aggregates/Administrator-profile.ts
+++ src/profiles/domain/model/aggregates/Administrator-profile.ts
@@ -1,18 +1,28 @@
 import {Profile} from "./profile";
-import {Column, Entity, BeforeInsert} from "typeorm";
+import {Column, Entity, BeforeInsert, CreateDateColumn, UpdateDateColumn} from "typeorm";
 import { CreateAdministratorProfileCommand } from '../commands/create-administrator-profile.command';
 import { UpdateAdministratorPersonalInformationCommand } from '../commands/update-administrator-personal-information.command';
 
 @Entity()
 export class AdministratorProfile extends Profile{
 
+    @Column({ name: 'hotel_id', nullable: false })
+    hotelId: number;
+
+    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+    updatedAt: Date;
 
     constructor() {
         super();
     }
 
-    static ConstructAdministratorProfileFromCommand(command: CreateAdministratorProfileCommand): AdministratorProfile {
+
+    static ConstructAdministratorProfileFromCommand(command: CreateAdministratorProfileCommand, hotelId:number): AdministratorProfile {
         const adminProfile = new AdministratorProfile();
+        adminProfile.hotelId = hotelId;
         Object.assign(adminProfile, Profile.create.call(this,
             null,
             command.name,
@@ -24,17 +34,16 @@ export class AdministratorProfile extends Profile{
         return adminProfile;
     }
 
-    static ConstructAdministratorProfileFromUpdateCommand(command: UpdateAdministratorPersonalInformationCommand): AdministratorProfile {
-        const adminProfile = new AdministratorProfile();
-        Object.assign(adminProfile, Profile.create.call(this,
-            null,
-            command.name,
-            command.lastName,
-            command.dni,
-            command.email,
-            command.phoneNumber
-        ));
-        return adminProfile;
+    public UpdatePersonalInformation(command: UpdateAdministratorPersonalInformationCommand): void {
+        this.name = command.name;
+        this.lastName = command.lastName;
+        this.dni = command.dni;
+        this.email = command.email;
+        this.phoneNumber = command.phoneNumber;
+    }
+
+    public setAccountId(accountId: number): void {
+        this.accountId = accountId;
     }
 
 }
